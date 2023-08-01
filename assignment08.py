ece0a56c5668eb6ea02189b341ae2237a867d306
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-08-01 15:46:33 +0000

    age cal

--- assignment08.py
+++ assignment08.py
@@ -1,4 +1,14 @@
-website = advaitarolkargmail.com
+# Add
+a=0
+for num in range(1,11):
+    a=num+a
+    print(a,end=' ')
 
-for i in website:
-    print(i)
+
+#multiplication table
+
+Num = int(input("Enter the number you want to generate a multiplication table for, then hit the `enter` key: "))
+Range = range(1,11)
+for x in Range:
+    result = Num * x
+    print(Num," * ",x," = ",result)
