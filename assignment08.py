9194245049e6ed954c7dedefe5f26a4d8a8b0ecd
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-08-03 15:34:48 +0000

    update day 7

--- assignment08.py
+++ assignment08.py
@@ -7,12 +7,27 @@ for num in range(1,11):
 
 #multiplication table
 
-Num = int(input("Enter a number"))
+Num = int(input("Enter a number "))
 for x in range(1,10+1):
     result = Num * x
     print(Num," * ",x," = ",result)
 
 
 #counting alphabets 
-a1=str(input('Enter a word'))
+num1=0
+name=str(input('Enter a word '))
+for a in name:
+    num1 = num1+1
+    print("Your total number of wrods is ",num1 )
 
+
+#counting number
+a2=0
+total= 0
+sum=int(input("Enter Numbers "))
+for i in sum:
+    total= total +1 
+
+
+
+#reverse number
