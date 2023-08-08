e729a9b055fdbde6d91eed180d02dd8ef4169358
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-08-08 15:41:02 +0000

    main

--- assignment09(mcq's).py
+++ assignment09(mcq's).py
@@ -1,3 +1,36 @@
-password = str(input("Set Your Password = "))
-
+s_pwd = str(input("Set Your Password = "))
 print('**---Your Pasword is Set Successfully---**')
+n_pwd = str(input("Enter Your Password = "))
+
+
+
+if s_pwd == n_pwd:
+    print("1. Who invented Java Programming? \n1.  Guido van Rossum\n 2.  James Gosling\n 3.  Dennis Ritchie\n 4.  Bjarne Stroustrup")
+    answer0 = int(input('Select Answer  only number - '))
+    if answer0 == 2:
+        print(" 2. Which component is used to compile, debug and execute the java programs?\n 1.  JRE  \n 2.  JIT  \n 3.  JDK \n 4.  JVM")
+        answer1 = int(input('Select Answer only number - '))
+        if answer1 == 3:
+            print('Your are Passed ')
+    else:
+        answer0 != 2
+        print('Try Next Year')
+elif s_pwd != n_pwd:
+        print('Enter One more time - 2/1')
+        n_pwd = input('Enter Your Password - ')
+        if s_pwd != n_pwd:
+            print('Enter One more time - 2/2')
+            n_pwd = input('Enter Your Password - ')
+        else:
+            if s_pwd == n_pwd:
+                print("1. Who invented Java Programming? \n 1.  Guido van Rossum\n 2.  James Gosling\n 3.  Dennis Ritchie\n 4.  Bjarne Stroustrup")
+                answer0 = int(input('Select Answer  only number - '))
+                if answer0 == 2:
+                    print(" 2. Which component is used to compile, debug and execute the java programs?\n 1.  JRE  \n 2.  JIT  \n 3.  JDK \n 4.  JVM")
+                    answer1 = int(input('Select Answer only number - '))
+                    if answer1 == 3:
+                        print('Your are Passed ')
+                else:
+                    answer0 != 2
+                    print('Try Next Year')      
+
