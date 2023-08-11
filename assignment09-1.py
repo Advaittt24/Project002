6620a3483e021cf4f28538dc617f0b7745664403
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-08-11 14:07:14 +0000

    little update

---
+++ assignment09-1.py
@@ -0,0 +1,20 @@
+s_pwd = str(input("Set Your Password = "))
+print('**---Your Pasword is Set Successfully---**')
+n_pwd = str(input("Enter Your Password = "))
+
+if s_pwd == n_pwd:
+    print()
+    print("1. Who invented Java Programming? \n1.  Guido van Rossum\n 2.  James Gosling\n 3.  Dennis Ritchie\n 4.  Bjarne Stroustrup")
+    answer0 = int(input('Select Answer  only number - '))
+    if answer0 == 2:
+        print(" 2. Which component is used to compile, debug and execute the java programs?\n 1.  JRE  \n 2.  JIT  \n 3.  JDK \n 4.  JVM")
+        answer1 = int(input('Select Answer only number - '))
+        if answer1 == 3:
+            print('Your are Passed ')
+        else:
+             answer1 == 3
+             print('Try Next Year')
+    else:
+        answer0 == 2
+        print('Try Next Year')
+elif s_pwd == n_pwd:
