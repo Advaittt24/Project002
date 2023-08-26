26f53467d7bca85b03c4c2aa1951067b9e35e22f
Advaittt24 <135209731+Advaittt24@users.noreply.github.com>
2023-08-26 15:31:40 +0000

    little update

---
+++ assignment11.py
@@ -0,0 +1,11 @@
+print("Welcome to Savita's Kitchen")
+
+product1 = str(input("Enter Your Product"))
+print("Your order is = ",product1)
+a1 = str(input("Do You Want Anything More [Y/N] = "))
+Y = "Y"
+N = "N"
+
+if a1 == Y:
+    product2 = str(input("Enter Your Product"))
+    print("Your Also order",product2)
